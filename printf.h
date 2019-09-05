9db84c86d94cf6463f9277b4da1b7a9207cce02d
Fernande Jenae <fjenae@vo-c2.21-school.ru>
2019-09-05 21:46:18 +0300

    0.008

--- printf.h
+++ printf.h
@@ -6,14 +6,16 @@
 /*   By: fjenae <fjenae@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/29 19:38:04 by fjenae            #+#    #+#             */
-/*   Updated: 2019/09/05 19:49:17 by fjenae           ###   ########.fr       */
+/*   Updated: 2019/09/05 21:33:52 by fjenae           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PRINTF_H
 # define PRINTF_H
 # include "./libft/libft.h"
+# include <stdarg.h>
+# include <stdio.h>
 
-void	ft_printf(int	k);
+void	ft_printf(int	k, ...);
 
 #endif
--- sources_core/printf.c
+++ sources_core/printf.c
@@ -6,18 +6,26 @@
 /*   By: fjenae <fjenae@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/29 19:36:05 by fjenae            #+#    #+#             */
-/*   Updated: 2019/09/05 19:47:01 by fjenae           ###   ########.fr       */
+/*   Updated: 2019/09/05 21:44:44 by fjenae           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-void	ft_printf(int	k)
+void	ft_printf(int	k, ...)
 {
-	k = 0;
-	while (k != 10)
+	va_list	args;
+	int		value_pulled_from_ft;
+	int		i;
+
+	i = 0;
+	va_start(args, k);
+	while (i < k)
 	{
-		ft_putnbr(k);
-		k++;
+		value_pulled_from_ft = va_arg(args, int);
+		// printf("number of variable %d: value of variable %d\n", i, value_pulled_from_ft);
+		printf("%d\n", value_pulled_from_ft);
+		i++;
 	}
+	va_end(args);
 }

670f52232f87574c7bf1ae6f1cc96ebb4feae167
Clement Missy <cmissy@at-h5.21-school.ru>
2019-09-16 17:13:58 +0300

    new struct with comments

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -9,13 +9,21 @@
 
 typedef struct		s_list
 {
-	long int		parameter;
-	char			flags; // или  enum !!!!!!!!!!!!!!!!!!!!!!!!!!!!! исправ!
-	long int		width; // узнать
-	long int		precision; // тоже
-	char			*lenght;
-	char			type; //OK
-	void			*arg;
+	unsigned int	parameter;// помни что порядок наследуется (см. тетрадь)
+	char			flags;//OK 5 вариантов, хватит 3х битов
+
+	int				width;//OK хватит границ инта
+	int				n_arg_width;//OK если есть '*', то > 0	
+
+	long int		precision;
+	int				n_arg_precision;//OK если есть '*', то > 0	
+
+	char			lenght;//OK побитово 4 варианта
+	char			type;//OK 11 вариантов, хватит 4х битов
+
+	char			*arg;//OK будем хранить строку
+	int				n_arg;
+	
 	struct s_list	*next;
 }					a_list;
 
--- main.c
+++ main.c
@@ -1,55 +1,12 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-char	ft_put_letter(unsigned int remainder, int X)
-{
-	char 	l[] = "abcdef";
-	char 	L[] = "ABCDEF";
-
-	if (X)
-		return (L[remainder - 10]);
-	return (l[remainder - 10]);
-}
-
-unsigned int	ft_uns_len(unsigned long long nb, int base)
-{
-	unsigned int	ln;
-	
-	ln = 1;
-    while (nb /=  base)
-		ln++;
-	return (ln);
-}
-
-char	*ft_uns_itoa_base_wregister(unsigned long long n, unsigned int base, int X)
-{
-	char					*result;
-	unsigned int			len;
-	unsigned int			remainder;
-
-	if (n == 0)
-		return ("0\0");
-	len = ft_uns_len(n, base);
-	if (!(result = (char*)malloc(sizeof(char) * (len + 1))))
-		return (NULL);
-	result[len] = '\0';
-	while (n > 0)
-	{
-		if ((remainder = n % base) > 9)
-			result[--len] = ft_put_letter(remainder, X);
-		else
-			result[--len] = remainder + '0';
-		n /= base;
-	}
-	return (result);
-}
+#include "ft_printf.h"
 
 int		main(void)
 {	
-	long long	i = -922337203685477580;
+	// long long	i = 1;
+	float 		a = 1.723;
+	
+ 	printf("%.0f\n", a);
+	// printf("our itoa %s", ft_uns_itoa_base_wregister(i, 16, 0));
 
- 	printf("original %llx\n", i);
-	printf("our itoa %s", ft_uns_itoa_base_wregister(i, 16, 0));
 	return (0);
 }

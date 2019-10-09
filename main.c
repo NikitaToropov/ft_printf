b21ea1cdccd187c3be46d40fd8d569cab18e11be
Clement Missy <cmissy@ga-t3.21-school.ru>
2019-10-09 21:09:48 +0300

    trobl

--- main.c
+++ main.c
@@ -3,48 +3,18 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-void	put_long_double(void *c)
-{
-	unsigned char	*m;
-
-	m =
-}
-// {
-// 	unsigned char	scaner;
-// 	unsigned char	*m;
-// 	unsigned int	a = 1;
-
-// 	printf("%u\n", len);
-// 	m = (unsigned char*)c + len - 1;
-// 	while (m >= (unsigned char*)c)
-// 	{
-// 		scaner = 128;
-// 		while (scaner)
-// 		{
-// 			if (*m & scaner)
-// 				write(1, "1", 1);
-// 			else
-// 				write(1, "0", 1);
-// 			// if (a == || a == || a ==)
-// 			if (scaner == 16)
-// 				write(1, " ", 1);
-// 			// if (a == || a == || a ==)
-// 			scaner /= 2;
-// 		}
-// 		if (m != (unsigned char*)c)
-// 			write(1, " ", 1);
-// 		m--;
-// 	}	
-// }
+// 1\100 0000 ' 0000 0110 \1011 0010 ' 0010 0000 ' 0000 0000 ' 0000 0000 ' 0000 0000 ' 0000 0000 ' 0000 0000 ' 0000 0000
+// 		9			8		   7		   6		   5		   4		   3		   2		   1			0
 
 void	ft_print_bits(void *c, unsigned int len)
 {
 	unsigned char	scaner;
 	unsigned char	*m;
+
 	unsigned int	a = 1;
 
 	printf("%u\n", len);
-	m = (unsigned char*)c + 9;
+	m = (unsigned char*)c + len - 1;
 	while (m >= (unsigned char*)c)
 	{
 		scaner = 128;
@@ -66,6 +36,54 @@ void	ft_print_bits(void *c, unsigned int len)
 	}	
 }
 
+
+void	put_long_double(void *c)
+{
+	unsigned long long int		integer;
+	long long int				fractional;
+	short				exponent;
+	unsigned char				*m;
+	unsigned int				i;
+	
+	m = (unsigned char*)c;	
+	exponent = ((short)m[9] & 127) << 8 | (int)m[8];
+	// if (exponent >> 14)
+	// 	exponent = (exponent >> 14 << 15);
+	// else
+	// 	exponent = ((exponent & 32767) | (short)m[9]) - 32767; 
+	ft_print_bits(&exponent, sizeof(exponent));
+}
+
+
+// void	ft_print_bits(void *c, unsigned int len)
+// {
+// 	unsigned char	scaner;
+// 	unsigned char	*m;
+// 	unsigned int	a = 1;
+
+// 	printf("%u\n", len);
+// 	m = (unsigned char*)c + 9;
+// 	while (m >= (unsigned char*)c)
+// 	{
+// 		scaner = 128;
+// 		while (scaner)
+// 		{
+// 			if (*m & scaner)
+// 				write(1, "1", 1);
+// 			else
+// 				write(1, "0", 1);
+// 			// if (a == || a == || a ==)
+// 			if (scaner == 16)
+// 				write(1, " ", 1);
+// 			// if (a == || a == || a ==)
+// 			scaner /= 2;
+// 		}
+// 		if (m != (unsigned char*)c)
+// 			write(1, " ", 1);
+// 		m--;
+// 	}	
+// }
+
 int		main(void)
 {	
 	// char 			*str;
@@ -73,10 +91,14 @@ int		main(void)
 	// float			l = 3.4 * 10 ^ 38;
 	long double		c = -178.125;
 	// double		c = -178.125;
-	// int		c = 4991;
+	// short		c = -32768;
 
 	printf("%Lf\n", c);
-	ft_print_bits(&c, sizeof(c));
+	put_long_double(&c);
+
+	// printf("%hi\n", c);
+
+	// ft_print_bits(&c, sizeof(c));
 	// c = c<<1>>24;
 	// ft_print_bits(&c, sizeof(c));
 
@@ -95,4 +117,5 @@ int		main(void)
 
 // long double: ?? (sizof = 16 but real size 20)
 // 1100 0000 0000 0110 1011 0010 0010 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000
-// 1\100 0000 0000 0110 \1011 0010 0010 0000 ' 0000 0000 0000 0000 ' 0000 0000 0000 0000 ' 0000 0000 0000 0000
+// 1\100 0000 ' 0000 0110 \1011 0010 ' 0010 0000 ' 0000 0000 ' 0000 0000 ' 0000 0000 ' 0000 0000 ' 0000 0000 ' 0000 0000
+// 		9			8		   7		   6		   5		   4		   3		   2		   1			0
